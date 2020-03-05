ab03492d26d9f1a0e7d8e42b2d034f9019989f4d
QPeiran <quanpeiran@gmail.com>
2020-03-05 13:01:37 +1300

    Testing "Observale" here -- safe to delete

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from '../post.model'
 import { NgForm } from '@angular/forms';
 import { PostService } from 'src/app/posts.service';
+import {of} from 'rxjs';
 
 @Component({
   selector: 'app-post-create',
@@ -22,6 +23,25 @@ export class PostCreateComponent {
     console.dir(input);
     alert("Data Saved! (form post-create.component)");
     this.yigePost = input.value;
+//////////////////////////////// Testing "Observale" here -- safe to delete/////////////
+    // Create simple observable that emits three values
+    const myObservable = of(1, 2, 3);
+
+    // Create observer object
+    const myObserver = {
+      next: x => console.log('Observer got a next value: ' + x),
+      error: err => console.error('Observer got an error: ' + err),
+      complete: () => console.log('Observer got a complete notification'),
+    };
+
+    // Execute with the observer object
+    myObservable.subscribe(myObserver);
+    // Logs:
+    // Observer got a next value: 1
+    // Observer got a next value: 2
+    // Observer got a next value: 3
+    // Observer got a complete notification
+/////////////////////////////////////////////////////////////////////////////////////
   }
   onAddNewPost(){
     const posting: Post = {
