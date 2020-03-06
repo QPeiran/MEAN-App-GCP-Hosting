6c8ad9e2dfbde22951fd35e04248d23591afdba6
QPeiran <quanpeiran@gmail.com>
2020-03-06 16:31:16 +1300

    onDestory to unsubscribe

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -13,13 +13,13 @@ export class PostService {
     this.SomePosts.push(ANewPost);
     console.log("post.service ");
     console.log(this.SomePosts);
-    this.postsUpdated.next([...this.SomePosts]);
+    this.postsUpdated.next([...this.SomePosts]); //now this.subject performing as "Observer"
   }
 
-  postsUpdatedGetter() {
+  postsObservable() {
     console.log("getting Posts: ");
     console.log(this.SomePosts);
-    return this.postsUpdated.asObservable(); ///return this.subject
+    return this.postsUpdated.asObservable(); ///return this.subject as "Observable"
     //Subjects are observables themselves but what sets them apart is that they are also observers.
     //".asObservable()" return a good old Observable version, will make your code much safer and will prevent poor coding practices
   }
--- src/app/posts/post-list/post-list.componet.ts
+++ src/app/posts/post-list/post-list.componet.ts
@@ -11,7 +11,7 @@ import { Subscription } from 'rxjs';
 export class PostListComponent1 implements OnInit, OnDestroy{
   @Input() posts1 : Post[] = [];
   posts2 : Post[] = [];
-  postsSub = new Subscription;
+  private postsSub = new Subscription;
 
   constructor (public postService: PostService){
     //"public" keywork automatically create a new property (postService)
@@ -20,11 +20,30 @@ export class PostListComponent1 implements OnInit, OnDestroy{
 
   ngOnInit() {
     //this.posts2 = this.postService.PostsGetter();
-    this.postService.postsUpdatedGetter() //call subscribe() method from "Observable" class
-      .subscribe((posts: Post[]) => {
-        //console.log(this.posts2);
-        this.posts2 = posts;
-      });
+    const myObservable = this.postService.postsObservable();
+    const myObserver =  (posts: Post[]) => {
+           console.log("Hi from myObserver!");
+           this.posts2 = posts;
+           console.log(this.posts2);}
+    ////////////////////////////////////////// not working
+    // const myObserver =  function (posts: Post[]) {
+    //   console.log("Hi!");
+    //   this.posts2 = posts;
+    //   console.log(this.posts2);}
+    // OR
+    // const myObserver = {
+    //   next: function (posts: Post[]) {
+    //     console.log("Hi!");
+    //     this.posts2 = posts;
+    //     console.log(this.posts2);
+    //   },
+    //   error: function (error){},
+    //   complete: function() {}
+    // };
+    //////////////////////////////////////////////
+    console.log(myObserver);//it only execute once when the "PostListComponent1" initialize
+     //call subscribe() method from "Observable" class
+     this.postsSub = myObservable.subscribe(myObserver);
   }
 
   ngOnDestroy() {
