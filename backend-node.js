5ccc94366704db3de04fc6c3720dccbbb06cd26f
QPeiran <quanpeiran@gmail.com>
2020-03-13 16:34:38 +1300

    routing at the backend

--- backend-node.js
+++ backend-node.js
@@ -3,16 +3,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
 
-app.use(express.static('dist/frontend-angular'));
+app.use('/', express.static('dist/frontend-angular'));
 
 app.get('/', (req,res) => {
   var options = {
     root: path.join(__dirname, 'dist/frontend-angular')
   }
-  return res.sendFile('index.html', options);
+  return res.status(200).sendFile('index.html', options);
 });
 
-
+app.get('/text', (req,res) => {
+  res.status(200).send('hello world');
+});
 
 app.listen(port, () => {
   console.log(`Server is running at PORT ${port}`);
