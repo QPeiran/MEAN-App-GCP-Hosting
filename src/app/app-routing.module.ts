80e1646ea228837831974e33aa326d6bdde2da4f
QPeiran <quanpeiran@gmail.com>
2020-03-13 17:13:39 +1300

    initializing memes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {
 
-  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent1 } from './posts/post-list/post-list.componet';
-import { PostService } from './posts.service';
 import { MemesComponent } from './memes/memes.component';
 
 @NgModule({
