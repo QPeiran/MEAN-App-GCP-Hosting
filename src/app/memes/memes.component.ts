af304bf4973152a82211238072d4eceb8df97b30
QPeiran <quanpeiran@gmail.com>
2020-03-16 14:43:28 +1300

    document.getElementById("pasteMemes").appendChild(image);

--- src/app/memes/memes.component.ts
+++ src/app/memes/memes.component.ts
@@ -11,5 +11,16 @@ export class MemesComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  API_URL: string = 'https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat';
+  ShowMeme() {
+    fetch(this.API_URL)
+    .then(response => response.json())
+    .then(function (json) {
+        var image = document.createElement('img');
+        image.src = json.data.image_url;
+        //document.body.appendChild(image);
+        console.log(json.data.image_url);
+        document.getElementById("pasteMemes").appendChild(image);
+      })
+  }
 }
