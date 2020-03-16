fdf90509572a98961f1c638babcd6c5f54cdcf71
QPeiran <quanpeiran@gmail.com>
2020-03-16 15:44:06 +1300

    adding two components -- dogmemes and catamemes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MemesComponent } from './memes/memes.component';
+import { DogmemesComponent } from './dogmemes/dogmemes.component';
 
 
 const routes: Routes = [
-
+  {path: 'catmemes', component: MemesComponent},
+  {path: 'dogmemes', component: DogmemesComponent},
 ];
 
 @NgModule({
@@ -11,3 +14,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const rountingComponents = [MemesComponent, DogmemesComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, rountingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent1 } from './posts/post-list/post-list.componet';
-import { MemesComponent } from './memes/memes.component';
-
+//import { DogmemesComponent } from './dogmemes/dogmemes.component';
+//import { MemesComponent } from './memes/memes.component';
+//now the "MemesComponents" will only be imported once & from "app-routing" module;
 @NgModule({
   declarations: [
     PostCreateComponent,
     AppComponent,
     HeaderComponent,
     PostListComponent1,
-    MemesComponent
+    rountingComponents,
+    //DogmemesComponent
+    //MemesComponent
   ],
   imports: [
     BrowserModule,
