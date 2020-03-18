4b2c7750f363a22f017cc48e7ebe9cb0aafd2e77
QPeiran <quanpeiran@gmail.com>
2020-03-19 10:07:14 +1300

    disconnect mysql for test

--- backend-node.js
+++ backend-node.js
@@ -36,35 +36,62 @@ app.listen(port, () => {
 module.exports = app;
 
 ////////////////////////////CloudSQL for MySQL part//////////////////////////
-var mysql = require('mysql');
+// var mysql = require('mysql');
+// var bodyParser = require('body-parser');
 
-var connection = mysql.createConnection({
-  host: 'localhost:3306',  /// <== change to CloudSQL for MySQL expose port
-  user: 'Peiran',  /// <== change
-  password: '',
-  database: 'scanning-database'
-});
-connection.connect();
-var event = {  //<== a dummy event for test
-  Barcode: '112A2$$$$$$$$0013010601',
-  Production_Batch: 1,
-  Recipe_and_P: "A2",
-  Timestamp: "6:29:24 AM",
-  Date: "3/12/2020",
-  Seq_Code: "0001 of 0106",
-  Week: 12,
-  Team_Leader: "iXUsr_Siti Rahmah Siregar 6092;",
-  Replenisher: "Dummy;",
-  Pickers: "Dummy;Dummy;Dummy;",
-  Notes: "",
-  Missing_Products:""
-};
+// app.use(bodyParser.json())
 
-var query = connection.query('insert into scanning-database ?', event, function(err, result) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.error(result);
-});
+// var connection = mysql.createConnection({
+//   host: 'localhost:3306',  /// <== change to CloudSQL for MySQL expose port
+//   user: 'Peiran',  /// <== change
+//   password: 'thepassword',
+//   database: 'scanning-database'
+// });
+// connection.connect(err => {
+//   if (err) {throw err;}
+//   else { console.log('Connected to MySQL'); }
+// });
+// var event = {  //<== a dummy event for test
+//   Barcode: '112A2$$$$$$$$0013010601',
+//   Production_Batch: 1,
+//   Recipe_and_P: "A2",
+//   Timestamp: "6:29:24 AM",
+//   Date: "3/12/2020",
+//   Seq_Code: "0001 of 0106",
+//   Week: 12,
+//   Team_Leader: "iXUsr_Siti Rahmah Siregar 6092;",
+//   Replenisher: "Dummy;",
+//   Pickers: "Dummy;Dummy;Dummy;",
+//   Notes: "",
+//   Missing_Products:""
+// };
+
+// connection.query('INSERT INTO scanning-database SET ?', event, function(error, result, field) {
+//   if (error) {
+//     //console.error(err);
+//     //return;
+//     throw error;
+//   }
+//   console.log(result);
+//   console.log(field);
+// });
+
+// app.post('/insert', function(req, res) {
+
+//   var jsondata = req.body;
+//   var values = [];
+
+//   for(var i=0; i< jsondata.length; i++)
+//     values.push([jsondata[i].name,jsondata[i].age]);
+
+//   //Bulk insert using nested array [ [a,b],[c,d] ] will be flattened to (a,b),(c,d)
+//   connection.query('INSERT INTO members (name, age) VALUES ?', [values], function(err,result) {
+//     if(err) {
+//        res.send('Error');
+//     }
+//    else {
+//        res.send('Success');
+//     }
+//   });
+//   });
 /////////////////////////////////////////end here///////
