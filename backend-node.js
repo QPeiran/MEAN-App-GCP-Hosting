86a3e5ed71c39bab6231b1ed2c9b3642923f7d6f
QPeiran <quanpeiran@gmail.com>
2020-03-18 13:53:55 +1300

    fetch calling from the backend

--- backend-node.js
+++ backend-node.js
@@ -1,21 +1,36 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
+const fetch = require("node-fetch");
 
 app.use('/', express.static('dist/frontend-angular')); ///middle-ware
 
 app.get('/', (req,res) => {
-  var options = {
+  let options = {
     root: path.join(__dirname, 'dist/frontend-angular')
   }
   return res.status(200).sendFile('index.html', options);
 });
 
-app.get('/text', (req,res) => {
-  res.status(200).send('hello world');
+app.get('/text', async (req,res) => {
+
+  const api_key = 'dc6zaTOxFJmzC';
+  const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${api_key}&tag=cat`)
+                               .then(res => res.json())
+                              //  .then(function (json) {
+                              //   console.log(json);
+                              // })
+  console.log("Response is : " + response);
+  res.status(200).send(response);
+  //const searchString = `q=${req.query.q}`;
+  //console.log(searchString.toString());
 });
 
 app.listen(port, () => {
   console.log(`Server is running at PORT ${port}`);
 });
+
+module.exports = app;
